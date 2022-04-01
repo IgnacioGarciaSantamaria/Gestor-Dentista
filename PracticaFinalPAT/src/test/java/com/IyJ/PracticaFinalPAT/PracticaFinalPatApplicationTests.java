15be5334af773dc9a6f1a4c2fac68a8fec50cb65
jaimedeclemente <78213320+jaimedeclemente@users.noreply.github.com>
2022-04-01 20:03:36 +0200

    SpringBoot

---
+++ PracticaFinalPAT/src/test/java/com/IyJ/PracticaFinalPAT/PracticaFinalPatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.IyJ.PracticaFinalPAT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticaFinalPatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
